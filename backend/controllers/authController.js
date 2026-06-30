b0c82ea7fbb1040b6dea30d4af13d3878601e378
Dhanshri-Nerkar <dhanshri.nerkar1904@gmail.com>
2026-06-30 10:15:35 +0530

    register otp issue solving

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,12 +15,17 @@ export const sendOtp = async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
